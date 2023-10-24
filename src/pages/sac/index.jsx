b213c46598fd5bfc18f49f505d6401adb187b641
CarlosAGToledo <carlos.gomes@souunit.com.br>
2023-10-24 17:09:53 -0300

    earcoddion fucional
    falta estilos
    
    Co-Authored-By: erickdanfrei <130263928+erickdanfrei@users.noreply.github.com>
    Co-Authored-By: vinicius-pascoal <112434231+vinicius-pascoal@users.noreply.github.com>

--- src/pages/sac/index.jsx
+++ src/pages/sac/index.jsx
@@ -1,35 +1,26 @@
-import Accordion from 'react-bootstrap/Accordion';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import Accordion from "../../assets/components/accordion";
 
-export default function Sac(){
-    return(
-        <>
-            <Accordion defaultActiveKey="0">
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-        </>
-    );
-}
+
+const Sac = () => {
+  const [items, setItems] = useState([
+    {
+      title: "Item 1",
+      content: "Este é o conteúdo do item 1.",
+    },
+    {
+      title: "Item 2",
+      content: "Este é o conteúdo do item 2.",
+    },
+  ]);
+
+  return (
+    <div>
+      {items.map((item) => (
+        <Accordion key={item.title} title={item.title} conteudo = {item.content} />
+      ))}
+    </div>
+  );
+};
+
+export default Sac;
