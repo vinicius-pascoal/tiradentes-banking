5816003dcd79ca0bb6e372a5c47ff3bdbab2cf46
vinicius-pascoal <vinicius.pascoal@raio.tech>
2023-10-24 01:50:29 -0300

    login fix

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -24,30 +24,24 @@ function Login() {
   };
 
   const handleSubmit = (event) => {
-    //Prevent page reload
     event.preventDefault();
 
     var { uname, pass } = document.forms[0];
 
-    // Find user login info
     const userData = database.find((user) => user.username === uname.value);
 
-    // Compare user info
     if (userData) {
       if (userData.password !== pass.value) {
-        // Invalid password
         setErrorMessages({ name: "pass", message: errors.pass });
       } else {
         setIsSubmitted(true);
         goTo("/dashboard", { replace: true });
       }
     } else {
-      // Username not found
       setErrorMessages({ name: "uname", message: errors.uname });
     }
   };
 
-  // Generate JSX code for error message
   const renderErrorMessage = (name) =>
     name === errorMessages.name && (
       <div className="error">{errorMessages.message}</div>
