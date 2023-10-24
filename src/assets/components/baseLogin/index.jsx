646b2f046801028f90f6fbe9842aca87a32fbd6d
vinicius-pascoal <vinicius.pascoal@raio.tech>
2023-10-23 22:21:47 -0300

    children corrigido

--- src/assets/components/baseLogin/index.jsx
+++ src/assets/components/baseLogin/index.jsx
@@ -2,12 +2,13 @@ import { Children } from "react";
 import frameLogo from "../../imgs/login/framelogoCortada.png";
 import fotoGarota from "../../imgs/login/garotacartaoCortada.png";
 
-function BaseLogin() {
+function BaseLogin({ children }) {
   return (
     <>
       <div class="contentLogin">
         <div class="areaLogin">
           <img src={frameLogo} alt="" />
+          {children}
         </div>
         <div class="imgLogin">
           <img src={fotoGarota} />
