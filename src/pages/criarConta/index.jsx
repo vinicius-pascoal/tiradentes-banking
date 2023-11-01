88b46ca41f30a836a16d463a17e5e1012524b8cd
Igor <igoralvesbf@gmail.com>
2023-10-31 21:48:25 -0300

    inputmask

--- src/pages/criarConta/index.jsx
+++ src/pages/criarConta/index.jsx
@@ -1,11 +1,29 @@
+import React, { useState } from 'react';
 import BaseLogin from "../../assets/components/baseLogin";
+import InputMask from 'react-input-mask';
+import { useForm, Controller } from "react-hook-form";
 
 function CriarConta() {
+  const [cpf, setCpf] = React.useState('')
+  const { register, handleSubmit, control } = useForm();
+  
   return (
     <>
       <BaseLogin>
         <form className="formularioLogin">
-          <input type="text" placeholder="insira seu CPF" />
+          <input
+              type="text"
+              placeholder="Insira seu CPF (000 000 000-00)"
+              mask="###.###.###-##"
+              value={cpf}
+              onChange={(e) => setCpf(e.target.value)}
+          />
+          <Controller
+            as={InputMask}
+            control={control}
+            mask="999.999.999-99"
+            name="cpf"
+          />
           <input type="text" placeholder="insira sua senha" />
           <button>criar conta</button>
         </form>
--- src/pages/recuperarConta/index.jsx
+++ src/pages/recuperarConta/index.jsx
@@ -1,12 +1,13 @@
 import BaseLogin from "../../assets/components/baseLogin";
+import React, { useState } from 'react';
 
 export default function RecuperarConta() {
   return (
     <>
       <BaseLogin>
         <form className="formularioLogin">
-          <input type="text" placeholder="insira seu CPF" />
-          <button>recuperar senha</button>
+        <input type="text" placeholder="Insira seu CPF (000 000 000-00)" mask="###.###.###-##" value="123.456.789-01" />
+          <button>Recuperar Senha</button>
         </form>
       </BaseLogin>
     </>
