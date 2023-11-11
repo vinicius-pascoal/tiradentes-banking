d16af4264a28e6d33c8d9cb045cfcd76d0c74222
Igor <igoralvesbf@gmail.com>
2023-11-11 17:40:03 -0300

    CriarConta inputmask

--- src/pages/criarConta/index.jsx
+++ src/pages/criarConta/index.jsx
@@ -1,31 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import BaseLogin from "../../assets/components/baseLogin";
-import InputMask from 'react-input-mask';
-import { useForm, Controller } from "react-hook-form";
+import { InputMask } from '@react-input/mask';
+
 
 function CriarConta() {
-  const [cpf, setCpf] = React.useState('')
-  const { register, handleSubmit, control } = useForm();
+  // const [cpf, setCpf] = React.useState('')
   
   return (
     <>
       <BaseLogin>
         <form className="formularioLogin">
-          <input
-              type="text"
-              placeholder="Insira seu CPF (000 000 000-00)"
-              mask="###.###.###-##"
-              value={cpf}
-              onChange={(e) => setCpf(e.target.value)}
-          />
-          <Controller
-            as={InputMask}
-            control={control}
-            mask="999.999.999-99"
-            name="cpf"
-          />
-          <input type="text" placeholder="insira sua senha" />
-          <button>criar conta</button>
+          <InputMask mask="___.___.___-__" 
+            replacement={{ _: /\d/ }}
+            placeholder="Insira seu CPF"
+            // onChange={(e) => setCpf(e.target.value)}
+            />
+            <input type="text" placeholder="Insira sua senha" />
+            <button>Criar Conta</button>
         </form>
       </BaseLogin>
     </>
