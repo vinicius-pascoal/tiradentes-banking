4788e8656c07c63b9ae3661230d65a60082da8de
vinicius-pascoal <vinicius.pascoal@raio.tech>
2023-11-13 09:14:21 -0300

    ajustes tela de academico

--- src/pages/academico/index.jsx
+++ src/pages/academico/index.jsx
@@ -1,79 +1,53 @@
+import propaganda from "../../assets/imgs/academico/propaganda.webp";
 
 export default function academico() {
   return (
     <div className="financeScream">
-      <div className="financeBox">
-        <h2>
-          financiamento de cursos e descontos
-        </h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-          voluptatum, quibusdam, quia, quod voluptatibus voluptatem accusamus
-          voluptates aperiam quos doloribus quae. Quisquam voluptatum, quibusdam,
-          quia, quod voluptatibus voluptatem accusamus voluptates aperiam quos
-          doloribus quae.
-        </p>
+      <div className="financeBox h-max">
+        <h2>financiamento de cursos e descontos</h2>
+        <div className="flex justify-center">
+          <img src={propaganda} alt="" srcset="" />
+        </div>
       </div>
-      <div className="financeBox">
-        <h2>
-          suas mensalidades
-        </h2>
+      <div className="financeBox h-max">
+        <h2>suas mensalidades</h2>
         <ul className="listaMensalidaes">
           <li>
             <span>parcela 1 </span>
-            <p>
-              valor: R$ 1.000,00
-            </p>
+            <p>valor: R$ 1.000,00</p>
           </li>
           <li>
             <span>parcela 2 </span>
-            <p>
-              valor: R$ 1.000,00
-            </p>
+            <p>valor: R$ 1.000,00</p>
           </li>
           <li>
             <span>parcela 3 </span>
-            <p>
-              valor: R$ 1.000,00
-            </p>
+            <p>valor: R$ 1.000,00</p>
           </li>
           <li>
             <span>parcela 4 </span>
-            <p>
-              valor: R$ 1.000,00
-            </p>
+            <p>valor: R$ 1.000,00</p>
           </li>
           <li>
             <span>parcela 4 </span>
-            <p>
-              valor: R$ 1.000,00
-            </p>
+            <p>valor: R$ 1.000,00</p>
           </li>
           <li>
             <span>parcela 4 </span>
-            <p>
-              valor: R$ 1.000,00
-            </p>
+            <p>valor: R$ 1.000,00</p>
           </li>
           <li>
             <span>parcela 4 </span>
-            <p>
-              valor: R$ 1.000,00
-            </p>
+            <p>valor: R$ 1.000,00</p>
           </li>
           <li>
             <span>parcela 4 </span>
-            <p>
-              valor: R$ 1.000,00
-            </p>
+            <p>valor: R$ 1.000,00</p>
           </li>
           <li>
             <span>parcela 4 </span>
-            <p>
-              valor: R$ 1.000,00
-            </p>
+            <p>valor: R$ 1.000,00</p>
           </li>
-
         </ul>
       </div>
     </div>
