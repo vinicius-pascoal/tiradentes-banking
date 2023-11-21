7cbba8e0516dc37716b1646ebe9381b7ecf9480d
vinicius-pascoal <vinicius.pascoal@raio.tech>
2023-11-21 02:06:10 -0300

    base changed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import browserslistToEsbuild from "browserslist-to-esbuild";
 
 export default defineConfig({
-  base: "/",
+  base: "/<REPO>/",
   plugins: [react()],
   build: {
     target: browserslistToEsbuild(),
