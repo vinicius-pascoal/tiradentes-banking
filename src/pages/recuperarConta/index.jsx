4ffb0528a7de3e0cd7fd9ab9234fb8a194ad0d4a
Igor <igoralvesbf@gmail.com>
2023-11-27 12:06:45 -0300

    input recuperarConta

--- src/pages/recuperarConta/index.jsx
+++ src/pages/recuperarConta/index.jsx
@@ -1,5 +1,6 @@
 import BaseLogin from "../../assets/components/baseLogin";
 import { useNavigate, Link } from "react-router-dom";
+import { InputMask } from '@react-input/mask';
 
 export default function RecuperarConta() {
   const goTo = useNavigate();
@@ -7,7 +8,10 @@ export default function RecuperarConta() {
   return (
     <BaseLogin>
       <form className="formularioLogin">
-        <input type="text" placeholder="insira seu CPF" />
+        <InputMask mask="___.___.___-__"
+          replacement={{ _: /\d/ }}
+          placeholder="insira seu CPF"/>
+        {/* <input type="text" placeholder="insira seu CPF" /> */}
         <button onClick={goTo("/")}>recuperar senha</button>
       </form>
     </BaseLogin>
